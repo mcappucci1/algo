5c49453ce74e830e9e1cc3a1ea8e942b85acb724
mcappucci34 <m.cappucci34@gmail.com>
2022-08-16 18:08:36 -0400

    Control algo and speed type from navbar

--- src/components/AlgoNavbar.tsx
+++ src/components/AlgoNavbar.tsx
@@ -1,27 +1,73 @@
-import { memo } from 'react';
+import '../css/AlgoNavbar.scss';
+import { memo, MouseEvent, useCallback, useMemo } from 'react';
+import { pathfindingAlgos, sortAlgos, speeds } from '../utils/defaultValues';
+import { NameValue } from '../utils/types';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 
-export const AlgoNavbar = memo(function AlgoNavbarInternal() {
+interface Props {
+    activeAlgo: NameValue;
+    setAlgo: (algo: NameValue) => void;
+    activeSpeed: NameValue;
+    setSpeed: (speed: NameValue) => void;
+}
+
+const createAlgoDropdowns = (objects: NameValue[], active: NameValue, onClick: (event: MouseEvent<HTMLElement>) => void) => {
+    return objects.map((object) => {
+        return (
+            <NavDropdown.Item
+                key={object.name}
+                value={object.value}
+                id={object.name}
+                active={active.name === object.name}
+                onClick={onClick}
+            >
+                {object.name}
+            </NavDropdown.Item>
+        );
+    });
+}
+
+export const AlgoNavbar = memo(function AlgoNavbarInternal({ activeAlgo, setAlgo, activeSpeed, setSpeed }: Props) {
+    const handleAlgoChange = useCallback((event: MouseEvent<HTMLElement>) => {
+        const name = event.currentTarget.getAttribute('id')!;
+        const value = event.currentTarget.getAttribute('value')!;
+        if (name === activeAlgo.name) return;
+        setAlgo({ name, value });
+    }, [activeAlgo]);
+
+    const handleSpeedChange = useCallback((event: MouseEvent<HTMLElement>) => {
+        const name = event.currentTarget.getAttribute('id')!;
+        const value = event.currentTarget.getAttribute('value')!;
+        if (name === activeSpeed.name) return;
+        setSpeed({ name, value });
+    }, [activeSpeed]);
+
+    const sortDropdowns = useMemo(() => createAlgoDropdowns(sortAlgos, activeAlgo, handleAlgoChange), [activeAlgo]);
+
+    const pathfindingDropdowns = useMemo(() => createAlgoDropdowns(pathfindingAlgos, activeAlgo, handleAlgoChange), [activeAlgo]);
+
+    const speedDropdowns = useMemo(() => createAlgoDropdowns(speeds, activeSpeed, handleSpeedChange), [activeSpeed]);
+
     return (
         <Navbar bg='dark' variant='dark'>
             <Container>
                 <Navbar.Brand href='#home'>Search and Sort</Navbar.Brand>
                 <Nav>
                     <NavDropdown title='Sort'>
-                        <NavDropdown.Item href='#action/3.1'>Sorting Algo</NavDropdown.Item>
+                        { sortDropdowns }
                     </NavDropdown>
-                    <NavDropdown title='Search' className='ms-2'>
-                        <NavDropdown.Item href='#action/3.1'>Search Algo</NavDropdown.Item>
+                    <NavDropdown title='Pathfinding' className='ms-2'>
+                        { pathfindingDropdowns }
                     </NavDropdown>
-                    <Button className='ms-5'>Start</Button>
-                    <Button className='ms-2'>End</Button>
                     <NavDropdown title='Speed' className='ms-2'>
-                        <NavDropdown.Item href='#action/3.1'>Slow</NavDropdown.Item>
+                        { speedDropdowns }
                     </NavDropdown>
+                    <Button className='ms-5' id='start-btn'>Start</Button>
+                    <Button className='ms-2' id='end-btn'>End</Button>
                 </Nav>
             </Container>
         </Navbar>
---
+++ src/utils/defaultValues.ts
@@ -0,0 +1,16 @@
+import { NameValue } from './types';
+
+export const sortAlgos: NameValue[] = [
+    { name: 'bubble-sort', value: 'Bubble Sort' },
+    { name: 'merge-sort', value: 'Merge Sort' },
+    { name: 'insert-sort', value: 'Insert Sort' }
+];
+export const pathfindingAlgos: NameValue[] = [
+    { name: 'dijkstras', value: 'Dijkstra\'s' }
+];
+export const speeds: NameValue[] = [
+    { name: 'Slow', value: '500' },
+    { name: 'Medium', value: '250' },
+    { name: 'Fast', value: '125' },
+    { name: 'Super Fast', value: '50' }
+];
